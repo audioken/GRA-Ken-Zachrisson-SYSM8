97db94aafe24c3d930f05d6c38db1cac676e78e3
audioken <kenzachrisson@gmail.com>
2025-05-19 00:31:24 +0200

    håller på med categoryfilter och få dom att reagera korrekt vid klick

--- drone-delights/src/components/CategoryFilter/CategoryFilter.jsx
+++ drone-delights/src/components/CategoryFilter/CategoryFilter.jsx
@@ -1,17 +1,38 @@
 import "./CategoryFilter.css";
+import { useState } from "react";
 
 function CategoryFilter() {
-    return (
-      <div className="category-filter-container">
-        <button className="category-btn all">All</button>
-        <button className="category-btn food">Food</button>
-        <button className="category-btn desserts">Desserts</button>
-        <button className="category-btn drinks">Drinks</button>
-        <button className="category-btn favourites">
-          <i className="fa-solid fa-heart"></i>
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const categories = ["All", "Food", "Desserts", "Drinks", "Favorites"];
+
+  function getButtonClass(category, selectedCategory) {
+    return `category-btn ${selectedCategory === category && selectedCategory !== "Favorites" ? "selected" : ""}`;
+  }
+  
+  function getIconClass(category, selectedCategory) {
+    return `fa-heart ${
+      selectedCategory === category ? "fa-solid fav-selected" : "fa-regular"
+    }`;
+  }
+  
+
+  return (
+    <div className="category-filter-container">
+      {categories.map((category) => (
+        <button
+          key={category}
+          className={getButtonClass(category, selectedCategory)}
+          onClick={() => setSelectedCategory(category)}
+        >
+          {category === "Favorites" ? (
+            <i className={getIconClass(category, selectedCategory)}></i>
+          ) : (
+            category
+          )}
         </button>
-      </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default CategoryFilter;
