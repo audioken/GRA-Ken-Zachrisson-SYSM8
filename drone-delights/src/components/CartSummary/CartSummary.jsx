6fb669b4bbc88ffe422ba36d116e050949629035
audioken <kenzachrisson@gmail.com>
2025-05-20 13:13:23 +0200

    stukturen klar för totalpriset för CartSummery

--- drone-delights/src/components/CartSummary/CartSummary.jsx
+++ drone-delights/src/components/CartSummary/CartSummary.jsx
@@ -1,7 +1,15 @@
 import "./CartSummary.css";
 
 function CartSummmary() {
-    return ( <div className="cart-summary-container"></div> );
+  return (
+    <div className="cart-summary-container">
+      <h2 className="cart-summary-title">
+        <span className="cart-summary-total">Total</span>
+        <span className="cart-summary-inc">(inc. VAT and fees)</span>
+      </h2>
+      <p className="cart-summary-price">${86.98}</p>
+    </div>
+  );
 }
 
 export default CartSummmary;
