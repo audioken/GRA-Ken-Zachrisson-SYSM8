a508aca4f788e61bb11877fe8a47fd89b43c6b29
audioken <kenzachrisson@gmail.com>
2025-05-20 11:52:43 +0200

    finjusterade MostOrderedList så man kan scrolla och använda expand på cards

--- drone-delights/src/components/MostOrderedList/MostOrderedList.jsx
+++ drone-delights/src/components/MostOrderedList/MostOrderedList.jsx
@@ -1,6 +1,7 @@
 import "./MostOrderedList.css";
 import ItemCard from "../ItemCard/ItemCard";
 import useFetch from "../../hooks/useFetch";
+import Masonry from "react-masonry-css";
 
 function MostOrderdList() {
   const { data, loading, error } = useFetch("http://localhost:3001/items");
@@ -14,17 +15,23 @@ function MostOrderdList() {
   return (
     <div className="most-ordered-container">
       <h2 className="most-ordered-title">Most Ordered</h2>
-      <div className="most-ordered-item-cards-container">
-        {mostOrderedItems.map((item) => (
-          <ItemCard
-            key={item.id}
-            id={item.id}
-            image={item.img}
-            name={item.name}
-            price={item.price}
-            description={item.description}
-          />
-        ))}
+      <div className="most-ordered-item-cards-container-scroll">
+        <Masonry
+          breakpointCols={{ default: 4}}
+          className="most-ordered-item-cards-container"
+          columnClassName="most-ordered-item-cards-column"
+        >
+          {mostOrderedItems.map((item) => (
+            <ItemCard
+              key={item.id}
+              id={item.id}
+              image={item.img}
+              name={item.name}
+              price={item.price}
+              description={item.description}
+            />
+          ))}
+        </Masonry>
       </div>
     </div>
   );
