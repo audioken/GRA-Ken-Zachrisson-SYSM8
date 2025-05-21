d528afd6cd6ffd8218f61e5699af9f42f3288c39
audioken <kenzachrisson@gmail.com>
2025-05-21 14:51:01 +0200

    finsjuterade hur marginaler funkar för pages och components, så komponenter är mer återanvändbara utan inbyggd margin

--- drone-delights/src/context/DeliveryContext.jsx
+++ drone-delights/src/context/DeliveryContext.jsx
@@ -4,12 +4,10 @@ export const DeliveryContext = createContext();
 
 export function DeliveryProvider({ children }) {
   const [deliveryInfo, setDeliveryInfo] = useState(() => {
-    // Hämta deliveryInfo från localStorage vid första renderingen
     const storedDeliveryInfo = localStorage.getItem("deliveryInfo");
     return storedDeliveryInfo ? JSON.parse(storedDeliveryInfo) : [];
   });
 
-  // Spara deliveryInfo i localStorage när de ändras
   useEffect(() => {
     localStorage.setItem("deliveryInfo", JSON.stringify(deliveryInfo));
   }, [deliveryInfo]);
